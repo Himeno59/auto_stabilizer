7fd396151f49828a4155d17a292218fa4f2c0dbb
himeno <himeno@jsk.imi.i.u-tokyo.ac.jp>
2024-10-18 14:36:24 +0900

    左手座標に対する重みを修正

--- auto_stabilizer/rtc/AutoStabilizer/FullbodyIKSolver.cpp
+++ auto_stabilizer/rtc/AutoStabilizer/FullbodyIKSolver.cpp
@@ -94,7 +94,7 @@ bool FullbodyIKSolver::solveFullbodyIK(double dt, const GaitParam& gaitParam,
     } else if (i==2) {
       this->ikEEPositionConstraint[i]->weight() << 3.0, 3.0, 3.0, 3.0, 3.0, 3.0;
     } else if (i==3) {
-      this->ikEEPositionConstraint[i]->weight() << 1.0, 1.0, 1.0, 1.0, 1.0, 1.0;
+      this->ikEEPositionConstraint[i]->weight() << 3.0, 3.0, 3.0, 3.0, 3.0, 3.0;
     }
     this->ikEEPositionConstraint[i]->eval_link() = nullptr;
     this->ikEEPositionConstraint[i]->eval_localR() = this->ikEEPositionConstraint[i]->B_localpos().linear();
